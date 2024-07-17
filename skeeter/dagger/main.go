0f54d4713b0b2fdf0d7d7e573e3eba069fee2048
Josh Ghiloni <ghiloni@gmail.com>
2024-07-16 23:43:03 -0400

    Update documentation

--- skeeter/dagger/main.go
+++ skeeter/dagger/main.go
@@ -1,16 +1,8 @@
-// A generated module for Skeeter functions
+// Add the ability to post to the Bluesky social network
 //
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
+// This adds the ability to post to Bluesky (aka skeets). It will parse
+// URLs from post text and add links if desired, and will upload images
+// as well
 
 package main
 
